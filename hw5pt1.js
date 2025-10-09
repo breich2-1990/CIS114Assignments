794da4e18e40761e695256e74d67f0e25de73243
breich2-1990 <breich2@my.smccd.edu>
2025-10-09 14:17:53 -0700

    hw 5

---
+++ hw5pt1.js
@@ -0,0 +1,60 @@
+var nameRes = null, radRes = null, selRes = null, string = "";
+
+
+window.onload = function () {
+
+
+
+
+
+    document.getElementById("submit").addEventListener("click", display);
+
+    document.getElementById('reset').addEventListener('click', function () {
+        location.reload();
+    });
+
+}
+
+function display() {
+
+
+    var check;
+    var checkRes = [];
+    var count = 0;
+
+    //retrieve
+    nameRes = document.getElementById("name").value;
+    radRes = personalInfo.elements["age"].value;
+    selRes = personalInfo.movieChoice.options[personalInfo.movieChoice.selectedIndex].value;
+    check = document.querySelectorAll('input[name="browser"]:checked');
+
+    check.forEach(checkbox => {
+        checkRes.push(checkbox.value);  //i included this because it makes sense for this code, even though you didn't require to display
+        count++;
+    });
+
+    if (document.querySelector('input[name="age"]:checked') == null) { //age
+        document.getElementById("result").value = "Please choose an age.";
+    } else if (nameRes == "") {  // name
+        document.getElementById("result").value = "Please enter a name.";
+    } else if (count == 0) {  //browser
+        document.getElementById("result").value = "Please choose a browser.";
+    } else if (selRes == "none") {  //movie
+        document.getElementById("result").value = "Please choose a movie type.";
+    } else {
+
+        string = "Name: " + nameRes + "\n";
+        string += "Age: " + radRes + "\n";
+        string += "Movie: " + selRes + "\n";
+        string += "Browser: " + checkRes + "\n";
+        string += "\nThanks, your data was submitted!"
+        document.getElementById("result").value = string;
+
+    }
+
+
+
+}
+
+
+
---
+++ hw5pt2.js
@@ -0,0 +1,48 @@
+
+
+window.onload = function () {
+
+    document.getElementById("submit").addEventListener("click", display);
+
+    document.getElementById('reset').addEventListener('click', function () {
+        location.reload();
+    });
+}
+
+function display() {
+
+    //my 2dim array
+    var stateArr = [
+        ["AL", "Alabama", "Montgomery", 5157699],
+        ["AK", "Alaska", "Juneau", 740133],
+        ["AZ", "Arizona", "Phoenix", 7582384],
+        ["AR", "Arkansas", "Little Rock", 3088354],
+        ["CA", "California", "Sacramento", 39431263],
+        ["CO", "Colorado", "Denver", 5957493]
+    ];
+
+    var stringResult = "", boolFound = false;
+
+
+
+    var input = document.getElementById("input").value;
+
+    for (i = 0; i < stateArr.length; i++) {
+        for (j = 0; j < stateArr.length; i++) {
+            if (input.toLowerCase() == stateArr[i][0].toLowerCase() || input.toLowerCase() == stateArr[i][1].toLowerCase()) {
+                stringResult = "State Abbreviation: " + stateArr[i][0] + "\n";
+                stringResult += "State Full Name: " + stateArr[i][1] + "\n";
+                stringResult += "State Capital: " + stateArr[i][2] + "\n";
+                stringResult += "State Population: " + stateArr[i][3].toLocaleString() + "\n";
+                document.getElementById("result").value = stringResult;
+                boolFound = true;
+            } else if (input == "") {
+                document.getElementById("result").value = "Please enter a state.";
+            } else if (boolFound == false) {
+                document.getElementById("result").value = "Sorry, we do not have information about this state! We only have information about Alabama, Alaska, Arizona, Arkansas, California, or Colorado.";
+            }
+        }
+    }
+
+
+}
